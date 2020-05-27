fb1c41a70d944e29c9f27581c77232fa05f47d03
Lukasz <lukaszgutowski@gmail.com>
2020-05-27 13:42:13 +0200

    ABG Diagnoser v1.4

--- app/src/main/java/com/lukaszgutowski/rkz_0_21/activity_caution.java
+++ app/src/main/java/com/lukaszgutowski/rkz_0_21/activity_caution.java
@@ -12,6 +12,7 @@ public class activity_caution extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caution);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
